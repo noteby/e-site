12771677a939c518cd7d050d13b9e6c9ffbc0d48
hatiu <hao.liu.xxx@gmail.com>
2022-12-05 15:54:52 +0800

    Reset api doc

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 #
 from .deps import filter_request_payload
 from .middleware import middleware
+from .openapi import reset_api_doc
 from .routers import router
 from .setup import setup
 from .utils.errors.handlers import exception_handlers
@@ -9,6 +10,10 @@ from .utils.errors.handlers import exception_handlers
 api = FastAPI(
     debug=False,
     version='0.1.0',
+    title='E-Api',
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
     middleware=middleware,
     dependencies=[Depends(filter_request_payload)],
     exception_handlers=exception_handlers,
@@ -20,5 +25,7 @@ async def startup_event():
     # Setup
     if not setup():
         raise RuntimeError('Setup error')
+    # Doc
+    reset_api_doc(api)
     # Router
     api.include_router(router.v1)
---
+++ backend/openapi.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Depends
+from fastapi.openapi.docs import get_redoc_html
+#
+from .deps import basic_auth
+
+
+def reset_api_doc(api: FastAPI):
+    openapi_url = '/doc/openapi.json'
+
+    @api.get(openapi_url,
+             include_in_schema=False,
+             dependencies=[Depends(basic_auth)],
+             )
+    def openapi_schema():
+        return api.openapi()
+
+    @api.get('/doc',
+             include_in_schema=False,
+             dependencies=[Depends(basic_auth)],
+             )
+    def doc_html():
+        return get_redoc_html(
+            openapi_url=openapi_url,
+            title='E Api | Doc'
+        )
