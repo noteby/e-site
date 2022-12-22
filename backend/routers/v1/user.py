aa0f438f834209148b2075e4845e3b0ac81d246a
hatiu <hao.liu.xxx@gmail.com>
2022-12-22 11:56:49 +0800

    Add response model for `user info req`

--- backend/routers/v1/user.py
+++ backend/routers/v1/user.py
@@ -7,6 +7,6 @@ from backend.internal.user import User
 router = APIRouter(tags=['User'])
 
 
-@router.get('/info')
+@router.get('/info', response_model=UserInfo)
 def user_info(user: User = Depends(current_user)):
     return UserInfo(email=user.email)
