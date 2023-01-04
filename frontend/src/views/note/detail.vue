250d5499372ef64ab77a7aec20a38a553f9ce1d5
hatiu <hao.liu.xxx@gmail.com>
2023-01-04 18:32:19 +0800

    Update style

--- frontend/src/views/note/detail.vue
+++ frontend/src/views/note/detail.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="px-3">
-    <div class="text-xl">{{ note['title'] }}</div>
-    <div class="mt-1 mb-3">
-      <div class="flex text-xs" v-if="$route.meta.requiresAuth">
-        <div class="mr-2">
-          <router-link
-              :to="{name:'EditNote',params: {noteId: $route.params.noteId}}"
-              class="underline"
-          >编辑
-          </router-link>
-        </div>
-        <div class="mr-2">
+  <div class="text-xl">{{ note['title'] }}</div>
+  <div class="mt-1 mb-3">
+    <div class="flex text-xs" v-if="$route.meta.requiresAuth">
+      <div class="mr-2">
+        <router-link
+            :to="{name:'EditNote',params: {noteId: $route.params.noteId}}"
+            class="underline"
+        >编辑
+        </router-link>
+      </div>
+      <div class="mr-2">
 
-          <a class="underline cursor-pointer"
-             @click="deleteDialogRef.deleteDialog=true">删除</a>
+        <a class="underline cursor-pointer"
+           @click="deleteDialogRef.deleteDialog=true">删除</a>
 
-          <DeleteDialog ref="deleteDialogRef"/>
-        </div>
+        <DeleteDialog ref="deleteDialogRef"/>
       </div>
-      <div class="flex text-xs">
-        <div class="text-gray-300">{{ formatDateStr(note['created_at']) }}</div>
-        <div class="text-gray-500 ml-2 italic" v-if="$route.meta.requiresAuth">
-          {{ note['display'] ? '显示' : '隐藏' }}
-        </div>
+    </div>
+    <div class="flex text-xs">
+      <div class="text-gray-300">{{ formatDateStr(note['created_at']) }}</div>
+      <div class="text-gray-500 ml-2 italic" v-if="$route.meta.requiresAuth">
+        {{ note['display'] ? '显示' : '隐藏' }}
       </div>
-      <div class="divide-y divide-solid divide-slate-200">
-        <div></div>
-        <div></div>
-      </div>
+    </div>
+    <div class="divide-y divide-solid divide-slate-200">
+      <div></div>
+      <div></div>
     </div>
   </div>
   <div>
     <Editor ref="editorRef" notEditable/>
   </div>
 </template>
+
+<style scoped>
+:deep(.ProseMirror) {
+  @apply px-0
+}
+
+</style>
 
 <script setup>
 import DeleteDialog from './my/deleteDialog.vue'
