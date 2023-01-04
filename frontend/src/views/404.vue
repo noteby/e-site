e733a4666f4b9b468bdb5046def035686e03f8e2
hatiu <hao.liu.xxx@gmail.com>
2023-01-04 09:36:02 +0800

    Add copyright for 404

--- frontend/src/views/404.vue
+++ frontend/src/views/404.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="pt-32"></div>
+  <div class="h-screen flex flex-col justify-between">
+    <div class="pt-32">
+      <el-result title="页面不存在" sub-title="">
+        <template #icon>
+          <el-image/>
+        </template>
 
-  <el-result title="页面不存在" sub-title="">
-    <template #icon>
-      <el-image/>
-    </template>
+        <template #extra>
+          <router-link class="e-back" to="/">回到首页</router-link>
+        </template>
 
-    <template #extra>
-      <router-link class="e-back" to="/">回到首页</router-link>
-    </template>
-
-  </el-result>
+      </el-result>
+    </div>
+    
+    <Copyright/>
+  </div>
 </template>
 
 <style scoped>
@@ -18,3 +22,7 @@
   @apply cursor-pointer text-gray-500 text-sm underline
 }
 </style>
+
+<script setup>
+import Copyright from "~/views/copyright.vue"
+</script>
