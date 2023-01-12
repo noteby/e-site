eb6114239a953a28bd7215ea76aef9e8aad28fdc
hatiu <hao.liu.xxx@gmail.com>
2023-01-12 16:23:09 +0800

    Update default desc

--- frontend/src/views/index/breadcrumb.vue
+++ frontend/src/views/index/breadcrumb.vue
@@ -1,7 +1,8 @@
 <template>
   <el-breadcrumb separator="/">
     <el-breadcrumb-item v-if="$route.meta.requiresAuth">管理</el-breadcrumb-item>
-    <el-breadcrumb-item v-else></el-breadcrumb-item>
-    <el-breadcrumb-item> {{ $route.meta.desc || '?' }}</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      {{ $route.meta.desc || '#' }}
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
